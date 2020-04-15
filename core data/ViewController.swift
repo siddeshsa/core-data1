f2f59d11dc4069e3fb2a3170ba2829f6aa392763
siddesh <siddeshsanaberu@gmail.com>
2020-04-15 16:24:03 +0530

    final

--- core data/ViewController.swift
+++ core data/ViewController.swift
@@ -8,13 +8,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource,UITableViewDelegate {
+    @IBOutlet weak var tableView: UITableView!
+    
+    var Items = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "Todos"
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)as UITableViewCell
+        cell.textLabel!.text = Items[indexPath.row]
+        
+        return cell
+    }
+    
+    
+    @IBAction func addItem(_ sender: Any) {
+        var alert = UIAlertController(title: "New Item", message: "Add a new Item", preferredStyle:.alert)
+        
+        let saveAction = UIAlertAction(title: "Save", style: .default)
+        { (action)-> Void in
+        
+            let textField = alert.textFields![0] as UITextField
+            self.Items.append(textField.text!)
+            self.tableView.reloadData()
+            
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+        alert.addTextField(configurationHandler: nil)
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+       
+      
+        
+        present(alert, animated: true, completion: nil)
+            
+            
+            
+    }
+    
 
 }
+
 
